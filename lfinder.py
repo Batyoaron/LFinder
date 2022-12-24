234178be5ad381e3d18a58a89f74d9f6216fd786
batyoaron <111697446+Batyoaron@users.noreply.github.com>
2022-12-24 22:34:45 +0100

    Update lfinder.py

--- lfinder.py
+++ lfinder.py
@@ -2,6 +2,9 @@
 import random
 import os
 import string
+import time
+import sys
+
 
 a = randomLetter = random.choice(string.ascii_letters)
 
@@ -25,9 +28,9 @@ print(ascii)
 
 
 print("\033[1;31;40m")
-print('                                       LFinder v2.0')
+print('                                       LFinder v2.2')
 print('                                       by: BatyoAron') 
-
+print('                                       rateing: 0.0')
 print("\033[1;30;40m \n")
 
 
@@ -48,12 +51,16 @@ else:
     if "http://" in link:
         print('check test 2: is good')
     else:
-        print("\033[1;31;40m \n")
-        print('bad link')
-        exit()
-
-ab = a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a + a                
+        if "www." in link:
+            print('check test 3: good')
+        else:
+            print("\033[1;31;40m \n")
+            print('bad link')
+            time.sleep(1)
+            os.execl(sys.executable, sys.executable, *sys.argv)           
         
+
+       
         
 print("\033[1;32;40m \n")
 hline = '''
@@ -71,6 +78,16 @@ if ".ngrok.io" in link:
             print('data: Ngrok links probably(80%) dangerous')
             exit()
             
+
+if "http://" in link:
+    
+    print("\033[1;33;40m")
+    print('risk: medium')
+    print('data: links that contain http instead of https it means that if hackers steal the website database, your password is getting leaked')
+    print('')
+    print('')
+    print('data 2:')    
+    print('')
     
 if ".com-" in link:
     if "@is.gd/" in link:
@@ -79,7 +96,18 @@ if ".com-" in link:
         print('data: Phishing')    
         exit()
         
-        
+if "trycloudflare.com" in link:
+    print("\033[1;31;40m")
+    print('risk: high')
+    print('Data: phishing')
+    exit()
+
+if "ihr.life" in link:
+    print("\033[1;31;40m")
+    print('risk: high')
+    print('Data: phishing') 
+    exit()
+                                                                              
 #ip logger
     
 if "wg.gl" in link:
@@ -183,10 +211,45 @@ if "ezstat.ru" in link:
     print('data: iplogger')
     exit()            
 
-if ab in link:
-    print("\033[1;31;40m")
-    print('risk: medium-high')
+
+
+######## links that hard to solve
+
+
+
+
+if "vm.tiktok.com/" in link:
+    print("\033[1;32;40m")
+    print('no risk')
+    print('data: Someone sent you a video link')
+    exit()
+else:
     
+    if "www.tiktok.com" in link:
+            print("\033[1;32;40m")   
+            print('no risk')
+            print('data: popular social media application')
+            exit()
+    
+    else:
+        
+        if "https://tiktok.com" in link:
+            print("\033[1;32;40m")
+            print('no risk')
+            print('data: popular social media application')
+            exit()
+        else:
+            if "tiktok" in link:
+                print("\033[1;31;40m")
+                print('risk: high')
+                print('data: if you get this link on messenger: do not open that link, the person who sent you the link it means he is infected with a virus')
+                exit()
+    
+            
+    
+    
+
+
 
 ############
 ###link is good #########
@@ -200,11 +263,10 @@ if link == "https://google.com":
     print('data: google is the most popular website / application in the world')
     exit()
     
-if "vm.tiktok.com/" in link:
-    print("\033[1;32;40m")
-    print('no risk')
-    print('data: Someone sent you a video link')
-    exit()
+
+
+
+
 
 if "https://www.facebook.com/profile.php?id=" in link:
     print("\033[1;32;40m")
@@ -232,8 +294,30 @@ if "www." in link:
         print("\033[1;32;40m")
         print('risk: low')
         print('data: paste it into google and it will tell you if it is dangerous or not')
-
+        exit()
+     
 ### no data
+      
+if "duckduckgo.com/" in link:
+          print("\033[1;30;40m")
+          print('private browser so it can be anything in that link, dont open it, come back in v4.0')
+          print('data: dont open that link. tor browser has dark web so everything can be in that link')
+          exit()
+          
+          
+      
+if ".onion" in link:
+        print("\033[1;30;40m")
+        print('tor browser dont supported, come back in v3.0')
+        print('data: dont open that link. tor browser has dark web so everything can be in that link')
+        exit()
+        
+
+
     
 print("\033[1;30;40m \n")
-print('no data yet, come back in a later version (:')      
+print('no data yet, come back in a later version (:')
+print("")
+input('enter to try again')
+os.execl(sys.executable, sys.executable, *sys.argv)            
+
